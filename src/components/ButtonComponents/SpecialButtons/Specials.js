3dce881395049f184c46403a6dcf67c515aeab57
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-16 15:57:20 -0600

    Modified: SpecialButtons components and ahve them displayed

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 //import any components needed
 import { specials } from '../../../data';
 //Import your array data to from the provided data file
+import SpecialButton from './SpecialButton';
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
