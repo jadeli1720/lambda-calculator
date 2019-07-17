a3926af295afe30e0ebe3a5f2147e4b8b9c1d56e
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-16 20:04:09 -0600

    Modified App.js and Numbers.js to bring the useStates to the top of the tree and use prop destructoring on the child components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
@@ -9,6 +9,7 @@ import Display from "./components/DisplayComponents/Display";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import { numbers } from "./data";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -16,7 +17,7 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
-
+  const [numberState, setNumberState] = useState(numbers);
   return (
     <div className="container">
       <Logo />
@@ -31,7 +32,7 @@ function App() {
             <Operators />
           </div>
           <div className="left-column">
-            <Numbers />
+            <Numbers numberState = {numberState} setNumberState = {setNumberState} />
             <Specials />
           </div>
         </div>
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -4,19 +4,19 @@ import { numbers } from '../../../data';
 //Import your array data to from the provided data file
 import NumberButton from './NumberButton';
 
-const Numbers = () => {
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
-  const [numberState, setNumberState] = useState(numbers);
-  console.log(numberState)
+  // const [numberState, setNumberState] = useState(numbers);
+  const { numberState, setNumberState} = props;
+  // console.log(numberState)
   return (
     <div className = "numbers-container">
         
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component */
-       numberState.map((props) => 
-       NumberButton(props))
-      }
+       numberState.map(props =>  NumberButton(props))
+       }
     </div>
   );
 };
